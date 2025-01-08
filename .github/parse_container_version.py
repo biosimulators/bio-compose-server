6cefaad4fe5bbb3f8e17c38bbcb7128d4159c0c6
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2025-01-08 13:52:27 -0500

    update images

--- .github/parse_container_version.py
+++ .github/parse_container_version.py
@@ -3,7 +3,7 @@ import sys
 
 
 service_name: str = sys.argv[1]
-is_dev: int = int(sys.argv[2])
+# is_dev: int = int(sys.argv[2])
 
 fp = 'docker-compose.yaml'
 
@@ -13,8 +13,8 @@ with open(fp, 'r') as file:
 image = compose_data['services'][service_name]['image']
 version = image.split(":")[1]
 
-if is_dev:
-    version += '-dev'
+# if is_dev:
+#     version += '-dev'
 
 print(version)
 
--- assets/docker/scripts/push_image.sh
+++ assets/docker/scripts/push_image.sh
@@ -4,13 +4,6 @@ set -e
 
 lib="$1"
 version="$2"
-# gh_username="$3"
-explicit_version=1
-
-if [ "$version" == "" ]; then
-  version=$(cat "$lib"/.VERSION)
-  explicit_version=0
-fi
 
 img_name=ghcr.io/biosimulators/bio-compose-server-"$lib"
 current_img="$img_name":"$version"
@@ -23,16 +16,3 @@ docker push "$current_img"
 docker tag "$current_img" "$latest_img"
 docker push "$latest_img"
 
-# handle version
-if [ "$lib" == "base" ]; then
-  VERSION_FILE=./assets/.BASE_VERSION
-else
-  VERSION_FILE=./"$lib"/.VERSION
-fi
-
-if [ "$explicit_version" == 1 ]; then
-  echo "Updating internal version of $lib with specified version."
-  echo "$version" > "$VERSION_FILE"
-fi
-
-echo "Updated version: $version"
